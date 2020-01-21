0758e6ab90b03fde031d49aa626103cd3682cd3e
Obi chinedu Frank <frankieetchy@gmail.com>
2020-01-21 18:37:59 +0100

    remove console log

--- content.js
+++ content.js
@@ -4,7 +4,6 @@ chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
       if( request.message === "clicked_browser_action" ) {
         const images = document.querySelectorAll('img');
-        console.log('image', images);
         images.forEach(img => {
             img.src = 'no_img.png'
             img.srcset = 'no_img.png'
